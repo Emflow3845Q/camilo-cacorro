6b72d0b6641062efafd81a71e2d8690634b47649
Emflow3845Q <128913713+Emflow3845Q@users.noreply.github.com>
2025-02-08 18:10:10 -0500

    CAmilo cacorron

--- backend/functions/convertImage.js
+++ backend/functions/convertImage.js
@@ -1,25 +1,38 @@
 import sharp from "sharp";
+import fs from "fs";
+import path from "path";
 
 export default function convertImage(format, req, res) {
   const imagePath = req.file.path;
   const imageName = path.basename(imagePath);
-  const renameImage = `storage/converted_image.${format}`;
-  console.log("Body: ", format, renameImage);
+  const renameImage = path.join("storage", `converted_image.${format}`);
+
+  console.log("Body:", format, renameImage);
 
   sharp(imagePath)
     .toFormat(format)
     .toFile(renameImage, (err, info) => {
       if (err) {
-        console.error(err);
+        console.error("Error al convertir la imagen:", err);
         return res.status(500).send("Error al convertir la imagen.");
       }
-      fs.unlinkSync(imagePath);
+
+      // Elimina la imagen original
+      fs.unlink(imagePath, (err) => {
+        if (err) console.error("Error al eliminar la imagen original:", err);
+      });
+
+      // Descarga la imagen convertida
       res.download(renameImage, (err) => {
         if (err) {
-          console.error(err);
+          console.error("Error al descargar la imagen:", err);
           return res.status(500).send("Error al descargar la imagen.");
         }
-        fs.unlinkSync(renameImage);
+
+        // Elimina la imagen convertida después de la descarga
+        fs.unlink(renameImage, (err) => {
+          if (err) console.error("Error al eliminar la imagen convertida:", err);
+        });
       });
     });
 }
